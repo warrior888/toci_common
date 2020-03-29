ae68579c6c0420283dcf12083cbf46a39a559237
Bartomiej Zapart <bzapart@hotmail.com>
2020-03-29 06:35:20 +0200

    draft of dbgeneric ef interfaces

---
+++ Toci.Libraries/Toci.Db/Interfaces/IGenericDbClient.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Toci.Db.Interfaces
+{
+    public interface IGenericDbClient<TModel>
+    {
+        IQueryable<TModel> Get(Expression<Func<string, List<TModel>>> where);
+
+        TModel Set(TModel model);
+    }
+}
---
+++ Toci.Libraries/Toci.Db/Interfaces/IGenericDbHandle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Toci.Db.Interfaces
+{
+    public interface IGenericDbHandle<TModel>
+    {
+        IQueryable<TModel> Select(Expression<Func<string, List<TModel>>> where);
+
+        TModel Insert(TModel model);
+
+        TModel Update(TModel model);
+
+        bool Delete(TModel model);
+    }
+}
